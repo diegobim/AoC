29425787d5f4d122f2b58cb81bd02767c529ba0d
Diego Bim <1907768+diegobim@users.noreply.github.com>
2023-12-02 04:58:52 -0300

    feat: 2023/2/1

---
+++ 2023/2/part1/main.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+const MAX_RED = 12
+const MAX_GREEN = 13
+const MAX_BLUE = 14
+
+var DigitRegex = regexp.MustCompile(`\d+`)
+
+type Set struct {
+	red   int
+	green int
+	blue  int
+}
+
+type Game struct {
+	id    int
+	sets  []Set
+	valid bool
+}
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	games := []Game{}
+	for scanner.Scan() {
+		line := scanner.Text()
+		games = append(games, parseGame(line))
+	}
+
+	validGameSum := 0
+	for _, game := range games {
+		if game.valid {
+			validGameSum += game.id
+		}
+	}
+
+	fmt.Println(validGameSum)
+}
+
+func parseGame(line string) Game {
+	parts := strings.Split(line, ":")
+	game := parseInt(DigitRegex.FindString(parts[0]))
+	sets := parseSets(strings.Split(parts[1], ";"))
+
+	return Game{
+		id:    game,
+		sets:  sets,
+		valid: validateSets(sets),
+	}
+}
+
+func parseSets(sets []string) []Set {
+	result := []Set{}
+
+	for _, set := range sets {
+
+		var red, green, blue int
+		for _, color := range strings.Split(set, ",") {
+			if strings.Contains(color, "red") {
+				red = parseInt(DigitRegex.FindString(color))
+			} else if strings.Contains(color, "green") {
+				green = parseInt(DigitRegex.FindString(color))
+			} else if strings.Contains(color, "blue") {
+				blue = parseInt(DigitRegex.FindString(color))
+			}
+		}
+
+		result = append(result, Set{
+			red:   red,
+			green: green,
+			blue:  blue,
+		})
+	}
+
+	return result
+}
+
+func validateSets(sets []Set) bool {
+	for _, set := range sets {
+		if set.blue > MAX_BLUE || set.green > MAX_GREEN || set.red > MAX_RED {
+			return false
+		}
+	}
+
+	return true
+}
+
+func parseInt(s string) int {
+	i, err := strconv.Atoi(s)
+	if err != nil {
+		panic(err)
+	}
+	return i
+}
