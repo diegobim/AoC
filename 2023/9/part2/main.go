98027bb47e1e906b0e459383be803220ee0b3c8a
Diego Bim <1907768+diegobim@users.noreply.github.com>
2023-12-09 14:04:22 -0300

    feat: 2023/9/2

--- 2023/9/part2/main.go
+++ 2023/9/part2/main.go
@@ -4,15 +4,67 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 
+	lines := []string{}
 	for scanner.Scan() {
 		line := scanner.Text()
-		fmt.Println(line)
+		lines = append(lines, line)
 	}
 
-	fmt.Println()
+	oasis := *parse(&lines)
+
+	var sum int64
+	for _, record := range oasis {
+		extrapolate(&record)
+		sum += record[0]
+	}
+
+	fmt.Println(sum)
+}
+
+func parse(lines *[]string) *[][]int64 {
+	oasis := [][]int64{}
+
+	for _, line := range *lines {
+		oasis = append(oasis, []int64{})
+		for _, char := range strings.Split(line, " ") {
+			oasis[len(oasis)-1] = append(oasis[len(oasis)-1], parseInt(char))
+		}
+	}
+
+	return &oasis
+}
+
+func extrapolate(record *[]int64) {
+	var allZero bool = true
+
+	diffs := []int64{}
+	for i := 1; i < len(*record); i++ {
+		curr := (*record)[i]
+		prev := (*record)[i-1]
+		diff := curr - prev
+		diffs = append(diffs, diff)
+
+		allZero = allZero && diff == 0
+	}
+
+	if !allZero {
+		extrapolate(&diffs)
+	}
+
+	*record = append([]int64{(*record)[0] - diffs[0]}, (*record)...)
+}
+
+func parseInt(s string) int64 {
+	i, err := strconv.ParseInt(s, 10, 64)
+	if err != nil {
+		panic(err)
+	}
+	return i
 }
